aaa1f52d80064d2029454afa40e5b90b73473f91
mukund-yr <31852513+mukund-yr@users.noreply.github.com>
2017-11-22 20:18:17 -0500

    Add files via upload
    new functions to get closest parking spots and display them.

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
-
+import googlemaps
+from haversine import haversine
+import os
+import jellyfish as jf
 from flask import render_template, request, flash, session, url_for, redirect, \
                   Blueprint
 from flask_login import current_user, login_required, login_user, logout_user
 from functools import wraps
 from app import db, app
 from forms import SignupForm, SigninForm, ContactForm, SellerForm, UpdateProfileForm, \
-                  UpdateParkingSpotForm, MessageForm, ApprovalForm
+                  UpdateParkingSpotForm, MessageForm, ApprovalForm, BuyerForm
 from models import User, Parking_Spot, Message
+import json 
+gmaps = googlemaps.Client(key='AIzaSyA3puSdjsWawVHB0LxKU7dk9s9bzHHteGU')
 
 # define the blueprint: 'parq', set url prefix: app.url/parq
 #app = Blueprint('parq', __name__, url_prefix='/parq')
@@ -108,11 +113,84 @@ def logout():
 @app.route('/buyer')
 @login_required
 def buyer():
+  lat_long = (40.810792, -73.960837)
   user = current_user
   uid = user.uid
-  allspots = Parking_Spot.query.filter(Parking_Spot.ownerid != uid, Parking_Spot.validity == 1).all()
-  
+  #allspots = Parking_Spot.query.filter(Parking_Spot.ownerid != uid, Parking_Spot.validity == 1).all()
+  allspots = Parking_Spot.query.filter(Parking_Spot.validity == 1).all()
+  new_spots = get_closest(lat_long, allspots)
+  allspots = new_spots
+
   return render_template('buyer.html', allspots=allspots)
+
+def get_mapbox_features(parking_spots):
+  features =[]
+
+  for spot in parking_spots:
+    feature = {}
+    feature['type'] = 'Feature'
+    
+    feature['geometry'] = {}
+    feature['geometry']['type'] = 'Point'
+
+    feature['properties'] = {}
+    feature['properties']['icon'] = 'circle'
+    feature['geometry']['coordinates'] = [float(spot.lon), float(spot.lat)]
+    feature['properties']['description'] = "<strong>"+ "Parking Spot : "+ str(spot.psid) +"</strong><p><a href=\"message/"+str(spot.psid)+"\" target=\"_blank\" title=\"Opens in a new window\">"+ spot.address + ", " + spot.city + ", " + spot.state + " " + str(spot.zipcode)+ "  :  " + spot.ps_size+  "</a></p>"
+
+    features.append(feature)
+
+  return features
+
+
+@app.route('/buyer_search',methods=['GET', 'POST'])
+@login_required
+def buyer_search():
+  form = BuyerForm(request.form)
+     
+  if request.method == 'POST':
+    user = current_user
+    uid = user.uid
+
+    if form.validate(uid) == False:  
+      return render_template('buyer_search.html', form=form)
+    
+    val_address = form.address.data+","+form.city.data+","+form.state.data+" "+str(form.zipcode.data)
+    if validate_address(val_address, gmaps):
+      val_add = validate_address(val_address, gmaps)
+      lat_long = tuple(val_add[1])
+    else:
+      flash('Invalid Address')
+      return render_template('buyer_search.html', form=form)    
+
+    allspots = Parking_Spot.query.filter(Parking_Spot.validity == 1).all()
+    new_spots = get_closest(lat_long, allspots)
+    allspots = new_spots
+    
+    features = get_mapbox_features(allspots)
+
+    feature = {}
+    feature['type'] = 'Feature'
+    
+    feature['geometry'] = {}
+    feature['geometry']['type'] = 'Point'
+
+    feature['properties'] = {}
+    feature['properties']['icon'] = 'harbor'
+    feature['geometry']['coordinates'] = [lat_long[1], lat_long[0]]
+    feature['properties']['description'] = "<strong>Requested Position</strong><p><a>"+ val_add[0] +  "</a></p>"
+
+    features = [feature] + features
+
+    center = features[0]['geometry']['coordinates']
+    features = json.dumps(features)
+    center = json.dumps(center)
+
+
+    return render_template('buyer.html',  mapbox_features = features, map_center = center )
+
+  return render_template('buyer_search.html', form=form)
+
 
 @app.route('/buyer_profile')
 @login_required
@@ -178,6 +256,31 @@ def viewspots():
   garage = Parking_Spot.query.filter_by(ownerid = uid, validity=1).all()
   return render_template('viewspots.html', garage=garage)
 
+def validate_address(val_address, gmaps):
+  response = gmaps.geocode(val_address)
+  #print response
+  if len(response) == 0 or ('partial_match' in response[0].keys() and response[0]['partial_match'] == True) :
+    return False
+  else:
+    coordinates = [response[0]['geometry']['location']['lat'], response[0]['geometry']['location']['lng']]
+    full_place_name = str(response[0]['formatted_address'])
+
+    return (full_place_name, coordinates)
+
+
+def get_closest(coordinates, parking_spots, radius = 1.0):
+  new_spots = []
+
+  for spot in parking_spots:
+
+    if haversine(coordinates,(float(spot.lat),float(spot.lon))) <= radius:
+      new_spots.append(spot)
+
+  
+  return new_spots
+
+
+
 
 @app.route('/addspots',methods=['GET', 'POST'])
 @login_required
@@ -191,7 +294,24 @@ def addspots():
     if form.validate(uid) == False:  
       return render_template('addspots.html', form=form)
     
-    parking_spot = Parking_Spot(uid, form.address.data, form.city.data, form.state.data, form.zipcode.data, form.ps_size.data)   
+    val_address = form.address.data+","+form.city.data+","+form.state.data+" "+str(form.zipcode.data)
+    if validate_address(val_address, gmaps):
+      val_add = validate_address(val_address, gmaps)
+      val_add1 = val_add[0]
+      val_add2 = val_add1.split(",")
+      valid_address = val_add2[0]
+      valid_city = val_add2[1]
+      val_add3 = val_add2[2].split(" ")
+      valid_state = val_add3[1]
+      valid_zipcode = int(val_add3[2])
+      val_location = val_add[1]
+      valid_latitude = val_location[0]
+      valid_longitude = val_location[1] 
+      parking_spot = Parking_Spot(uid, valid_address, valid_city, valid_state, valid_zipcode, form.ps_size.data, valid_latitude, valid_longitude)
+    else:
+      flash('Invalid Address')
+      return render_template('addspots.html', form=form)    
+  
     db.session.add(parking_spot)
     db.session.commit()
 
@@ -251,28 +371,34 @@ def delete_spot(parking_id):
 def update_spot(parking_id):
   form = UpdateParkingSpotForm(request.form)  
 
-  # Updates details of parking spot
-  #if request.method == 'POST':
-   # parking_spot.address = form.address
-    #parking_spot.city = form.city
-    #parking_spot.state = form.state
-    #parking_spot.zipcode = form.zipcode
-    #parking_spot.size = form.ps_size
-    #db.session.commit()
-
-    #return redirect(url_for('profile'))
-
-  # GET Method
-  # Query for this parking spot
   user = current_user
   parking_spot = Parking_Spot.query.filter_by(psid=parking_id, ownerid=user.uid).first()
 
   if request.method == 'POST':
-    parking_spot.address = form.address.data
-    parking_spot.city = form.city.data
-    parking_spot.state = form.state.data
-    parking_spot.zipcode = form.zipcode.data
+    val_address = form.address.data+","+form.city.data+","+form.state.data+" "+str(form.zipcode.data)
+    if validate_address(val_address, gmaps):
+      val_add = validate_address(val_address, gmaps)
+      val_add1 = val_add[0]
+      val_add2 = val_add1.split(",")
+      valid_address = val_add2[0]
+      valid_city = val_add2[1]
+      val_add3 = val_add2[2].split(" ")
+      valid_state = val_add3[1]
+      valid_zipcode = int(val_add3[2])
+      val_location = val_add[1]
+      valid_latitude = val_location[0]
+      valid_longitude = val_location[1] 
+    else:
+      flash('Invalid Address')
+      return render_template('update_spot.html', parking_spot=parking_spot, form=form)
+
+    parking_spot.address = valid_address
+    parking_spot.city = valid_city
+    parking_spot.state = valid_state
+    parking_spot.zipcode = valid_zipcode
     parking_spot.size = form.ps_size.data
+    parking_spot.lat = valid_latitude
+    parking_spot.lon = valid_longitude
     db.session.commit()
 
     return redirect(url_for('profile'))
